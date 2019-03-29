2eacab0668a0941240a0589263a888f2ab1afdc8
Brian Hensley <brnhensley@gmail.com>
2019-03-29 11:37:14 -0700

    add js functionality to read the value of select boxes, and consol.logs to print the value.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,14 @@
-console.log(typeof);
+$(document).ready(function() {
+  $("form#quiz").submit(function(event) {
+    var firstAnser = $("#q1").val();
+    console.log(firstAnswer);
+    console.log(secondAnswer);
+    console.log(thirdAnswer);
+    console.log(fourthAnswer);
+    console.log(fifthAnswer);
 
 
 
-
-    // Ruby/Rails
-    // PHP/Drupal
-    // Java/Android
-    // CSS/Design
-    // C#/.NET
+    event.preventDefault();
+  })
+})
