fe7fa4c05551736bc7c52a30efa7eedb9df6adac
Brian Hensley <brnhensley@gmail.com>
2019-03-29 11:51:10 -0700

    find the typo in my variables that made the whole page stop working

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,15 @@
 $(document).ready(function() {
-  $("form#quiz").submit(function(event) {
-    var firstAnser = $("#q1").val();
+  $("form").submit(function(event) {
+    var firstAnswer = $("#q1").val();
+    var secondAnswer = $("#q2").val();
+    var thirdAnswer = $("#q3").val();
+    var fourthAnswer = $("#q4").val();
+    var fifthAnswer = $("#q5").val();
     console.log(firstAnswer);
     console.log(secondAnswer);
     console.log(thirdAnswer);
     console.log(fourthAnswer);
     console.log(fifthAnswer);
-
-
 
     event.preventDefault();
   })
