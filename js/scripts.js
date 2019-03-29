ead19fea6857267773e98c3d69278f1a3cf68e8e
Brian Hensley <brnhensley@gmail.com>
2019-03-29 15:18:59 -0700

    add full responsiveness to any combination of answers.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+// questions are id'd "q1"-"q5" answers are class'd "a1"-"a5"
+
 $(document).ready(function() {
   $("form").submit(function(event) {
     var a1 = $("#q1").val();
@@ -6,22 +8,20 @@ $(document).ready(function() {
     var a4 = $("#q4").val();
     $("body").find("p").hide();
 
+// I'm sorry
     if (a1 === "noAnswer" || a2 === "noAnswer" || a3 === "noAnswer" || a4 === "noAnswer") {
       $("#klingon").show();
-    } else if (a1 === "csharp" && a2 === "csharp" && a3 === "csharp" && a4 === "csharp") {
+    } else if  (((a1 === "csharp" && a2 === "csharp") && ( a3 === "csharp" || a4 === "csharp" )) || ( a2 === "csharp" && a3 === "csharp" && a4 === "csharp" ) || ( a1 === "csharp" && a3 === "csharp" && a4 === "csharp" )) {
       $("#csharp").show();
-    } else if (a1 === "design" && a2 === "design" && a3 === "design" && a4 === "design") {
+    } else if  (((a1 === "design" && a2 === "design") && ( a3 === "design" || a4 === "design" )) || ( a2 === "design" && a3 === "design" && a4 === "design" ) || ( a1 === "design" && a3 === "design" && a4 === "design" )) {
       $("#design").show();
-    } else if (a1 === "java" && a2 === "java" && a3 === "java") {
+    } else if ((a1 === "java" && a2 === "java") || (a2 === "java" && a3 === "java") || (a1 === "java" && a3 === "java")) {
       $("#java").show();
-    }  else if (a1 === "php" && a2 === "php" && a3 === "php") {
+    } else if ((a1 === "php" && a2 === "php") || (a2 === "php" && a3 === "php") || (a1 === "php" && a3 === "php")) {
       $("#php").show();
-    }  else if (a1 === "ruby" && a2 === "ruby" && a3 === "ruby") {
+    } else if ((a1 === "ruby" && a2 === "ruby") || (a2 === "ruby" && a3 === "ruby") || (a1 === "ruby" && a3 === "ruby")) {
       $("#ruby").show();
-// I'm sorry
-    }  else if  (((a1 === "csharp" && a2 === "csharp") && ( a3 === "csharp" || a4 === "csharp" )) || ( a2 === "csharp" && a3 === "csharp" && a4 === "csharp" ) || ( a1 === "csharp" && a3 === "csharp" && a4 === "csharp" )) {
-      $("#csharp").show();
-    }  else if  (((a1 === "design" && a2 === "design") && ( a3 === "design" || a4 === "design" )) || ( a2 === "design" && a3 === "design" && a4 === "design" ) || ( a1 === "design" && a3 === "design" && a4 === "design" )) {
+    } else {
       $("#design").show();
     }
 
@@ -29,7 +29,7 @@ $(document).ready(function() {
     console.log(a2);
     console.log(a3);
     console.log(a4);
-
+    console.log("-----");
     event.preventDefault();
   })
 })
